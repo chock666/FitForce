908db8885bccd90e2b04ae30fd5542d8a6fd8e02
yehonatanf <hubmue123@gmail.com>
2024-03-26 14:50:19 +0200

    I suffered a lot

--- app/src/main/java/com/example/fitforce/HomeFragment.java
+++ app/src/main/java/com/example/fitforce/HomeFragment.java
@@ -1,16 +1,20 @@
 package com.example.fitforce;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
-public class HomeFragment extends Fragment {
-
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    Button swimmingButton, runningButton, strengthButton, fitnessButton;
+    View view;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -19,7 +23,45 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+
+         view = inflater.inflate(R.layout.fragment_home, container, false);
+        swimmingButton = view.findViewById(R.id.swimming_button);
+        runningButton = view.findViewById(R.id.running_button);
+        strengthButton = view.findViewById(R.id.Strength_button);
+        fitnessButton = view.findViewById(R.id.fitness_button);
+        strengthButton.setOnClickListener(this);
+        fitnessButton.setOnClickListener(this);
+
+        runningButton.setOnClickListener(this);
+        swimmingButton.setOnClickListener(this);
+        return view;
     }
+
+
+
+    @Override
+    public void onClick(View view) {
+        if (view == swimmingButton){
+            Intent switchActivityIntent = new Intent(getContext(), inSwimmingActivity.class);
+            startActivity(switchActivityIntent);
+        }
+        if (view == runningButton){
+            Intent switchActivityIntent = new Intent(getContext(), InRunningActivity.class);
+            startActivity(switchActivityIntent);
+        }
+        if (view == strengthButton){
+            Intent switchActivityIntent = new Intent(getContext(), InStrengthActivity.class);
+            startActivity(switchActivityIntent);
+        }
+        if (view == fitnessButton){
+            Intent switchActivityIntent = new Intent(getContext(), InFitnessActivity.class);
+            startActivity(switchActivityIntent);
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/fitforce/TrainingActivity.java
@@ -0,0 +1,27 @@
+package com.example.fitforce;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class TrainingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+    }
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
