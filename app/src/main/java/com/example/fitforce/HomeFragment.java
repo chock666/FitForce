12a7222153b0a714f81a282e416908f05279b0da
ADMIN <yehonatancf2006@gmail.com>
2024-07-02 12:34:15 +0300

    done

--- app/src/main/java/com/example/fitforce/HomeFragment.java
+++ app/src/main/java/com/example/fitforce/HomeFragment.java
@@ -1,6 +1,8 @@
 package com.example.fitforce;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -10,10 +12,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
     Button  runningButton, strengthButton, fitnessButton;
+    SharedPreferences sp;
+    TextView tvName;
     View view;
     public HomeFragment() {
         // Required empty public constructor
@@ -30,12 +35,17 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
          view = inflater.inflate(R.layout.fragment_home, container, false);
         runningButton = view.findViewById(R.id.running_button);
         strengthButton = view.findViewById(R.id.Strength_button);
+        tvName = view.findViewById(R.id.name);
         fitnessButton = view.findViewById(R.id.fitness_button);
         strengthButton.setOnClickListener(this);
         fitnessButton.setOnClickListener(this);
+        sp = requireActivity().getSharedPreferences("names", Context.MODE_PRIVATE);
+        tvName.setText(sp.getString("firstName",""));
 
         runningButton.setOnClickListener(this);
         return view;
+
+
     }
 
 
